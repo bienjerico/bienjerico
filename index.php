699373f2d4065982515fdb5942f03ed082a0d1ea
Bien Jerico Cueto <bien.jerico@gmail.com>
2014-10-18 22:42:55 +0800

    update info

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bien Jerico Cueto</title>
+    <title>bienjerico</title>
     <!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 </head>	
@@ -12,7 +12,192 @@
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 
-	<h1>Bien Jerico Cueto</h1>
+	<div class="text-center">
 
+			
+		<h3><span class="label label-default">About <b class="text-primary">bienjerico</b></span></h3>
+			<p>
+				Is twenty plus of age and five-nine tall with slim body type
+				<br/>
+				Is a graduate of Informatics Internation College, Northgate Alabang and currently working at Yondu
+				<br/>
+				Is interested in travelling around the world (dream), road trip, camping, island hopping, bar hopping and playing Dota One to Two and league of legends
+				<br/>
+				Is accepting layout, graphic, web, logo, invitation, t-shirt, car designs
+			</p>
+		
+		<h3><span class="label label-default">Skills</span></h3>
+			<p>
+				<b class="text-primary">Programming</b>
+			   	<br/>
+				HTML5, PHP 5, CSS3, Javascript, Ajax, JSON, jQuery, Shell Script, API
+			</p>
+
+			<p>
+				<b class="text-primary">Design</b>
+				<br/>
+				Adobe CS3 and CS5 (Photoshop, InDesign, Dreamweaver, Illustrator, After Effects, Premiere, Lightroom, Flash)
+			</p>
+
+			<p>
+				<b class="text-primary">Database</b>
+				<br/>
+				MySQL, MSSQL, Oracle: 9i, MS Access
+			</p>
+
+			<p>
+				<b class="text-primary">Tools</b>
+				<br/>
+				XAMPP, LAMP, IIS7
+				<br/>
+				Sublime Text 2, Netbeans 7, WebMatrix, Notepad++, MySQL Workbench
+				<br/> 
+				SVN and GIT (version control), PuTTY, FileZilla, WinSCP, VMware Player
+				<br/>
+				Microsoft SQL Server, Microsoft Visual Studio 2008 R2, Office Word, Excel, Vision, PowerPoint and Outlook
+			</p>
+
+			<p>
+				<b class="text-primary">OS</b>
+			   	<br/>
+				Window XP, 7, 8 and Ubuntu
+			</p>
+			
+
+
+
+
+		<h3><span class="label label-default">Experience</span></h3>
+		<p>	
+			More than 3 years experienced as a open source web application developer. 
+			<br/>
+			Background experienced in font-end/back-end object oriented programming with MySQL database. 
+			<br/>
+			My Objective is to learn more latest web technologies and integrate in different projects and environment.
+		</p>
+
+		<p>
+			<b class="text-primary">Yondu</b>
+			<br/>
+			<b>Software Engineer</b> September 2014 to Present <i>(1 month)</i>
+			<br/>
+			Developed and designed web application and database structure. Research and provide new technology for system development. 
+		</p>
+
+		<p>
+			<b class="text-primary">BPO International, Inc.</b>
+			<br/>
+			<b>Senior Software Engineer</b> February 2013 to August 2014 <i>(1 year and 7 months)</i>
+			<br/>
+			<b>Software Engineer</b> April 2011 to October 2012 <i>(1 year and 7 months)</i>
+			<br/>
+			<b>On the Job Training</b> February 2011 to April 2013 <i>(3 months)</i>
+			<br/>
+			Developed and designed web application and database structure. Troubleshoot and maintain the existing system. 
+			<br/>
+			Setup and administer database client databases. Provide system update report. Research new idea for system development. 
+			<br/>
+			Provide new functionalities to enhancement of system. Analyze the system flow. 
+		</p>
+
+		<p>
+			<b class="text-primary">Samsung Electronics Philippines Corporation (SEPCO)</b>
+			<br/>
+			<b>Web Developer</b> November 2012 to January 2013 <i>(3 months)</i>
+			<br/>
+			Troubleshoot in-house web application. Update existing system. Developed web application and designed user interface. 
+			<br/>
+			Create and designed database structure.
+		</p>
+
+
+		<h3><span class="label label-default">Projects</span></h3>
+		<p>
+			<b class="text-primary">Captive Portal</b> <b>Yondu</b>
+			<br/>
+			On going project...
+			<br/>
+			<h6><i>&nbsp;</i></h6>
+			<br/>
+
+			<b class="text-primary">Time and Attendance System</b> <b>BPO International, Inc.</b>
+			<br/>
+			Combination of Leave Management Module, Overtime Management Module, Schedule Management Module, Timekeeping Module. 
+			<br/>
+			System provide PDF, Excel reports and email notification in every transaction. 
+			<br/>
+			Client can access the system using internet.
+			<br/>
+			<h6><i>PHP 5 (PHP Native with Object Oriented Programming), HTML 5, CSS3 (Twitter Bootstrap), Javascript (Ajax and jQuery), MySQL, Apache, STMP</i></h6>
+			<br/>
+			
+			<b class="text-primary">Employee Connection (E-CON)</b> <b>BPO International, Inc.</b>
+			<br/>
+			System developed to show updated news, photos, birthday celebrant, employee feedback module, office directory and also connected to Time and Attendance System.
+			<br/>
+			<h6><i>PHP 5 (PHP Native with Object Oriented Programming), HTML 5, CSS3 (Twitter Bootstrap), Javascript (Ajax and jQuery), MySQL, Apache, STMP</i></h6>
+			<br/>
+			
+			<b class="text-primary">Online Report System</b> <b>BPO International, Inc.</b>
+			<br/>
+			System provide final report (Leaves, Balance, Man-Hours Work, Absenteeism, Pay Slip PDF and Credit Profile PDF) from Time and Attendance System. 
+			<br/>
+			Some of the client use ASP.NET for their company system, using SOAP can access the system.
+			<br/>
+			<h6><i>PHP 5 (PHP Native with Object Oriented Programming), HTML 5, CSS3, Javascript (Ajax and jQuery), MySQL, Apache</i></h6>
+			<br/>
+
+			<b class="text-primary">Account Payable and Expenses Report Monitoring System</b> <b>BPO International, Inc.</b>
+			<br/>
+			System developed to track and monitor the status of client invoice. 
+			<br/>
+			This system use by Receiver, Processor and Reviewer to process the invoice. 
+			<br/>
+			<h6><i>PHP 5 (PHP Native with Object Oriented Programming), HTML 5, CSS3, Javascript (Ajax and jQuery), MySQL, Apache</i></h6>
+			<br/>
+			
+			<b class="text-primary">Consumer Training Program</b> <b>SEPCO</b>
+			<br/>
+			Online training registration for the consumer.
+			<br/>
+			<h6><i>PHP 5 (ADOdb Library), HTML 5, CSS3, Javascript (Ajax and jQuery), MSSQL, IIS7 and Microsoft Visual Studio 2008 R2.</i></h6>
+
+		</p>
+
+
+		<h3><span class="label label-default">Certificate</span></h3>
+		<p>
+			<b class="text-primary">Microsoft Certified Professional</b>
+			<br/>
+			Programming in HTML5 with JavaScript and CSS3 Specialist <i>February 2013</i>
+		</p>
+
+		<p>
+			<b class="text-primary">Phoenix One - The Knowledge Institute</b>
+			<br/>
+			Effective Business Communication <i>September 2013</i>
+		</p>
+
+		<h3><span class="label label-default">Contact</span></h3>
+
+		<p>
+			<b class="text-primary">Skype</b>
+			<br/>
+			bien.jerico.cueto
+		</p>
+
+		<p>
+			<b class="text-primary">Email Address</b>
+			<br/>
+			bien.jerico@gmail.com
+		</p>
+		
+		<p>
+			<b class="text-primary">Address</b>
+			<br/>
+			Philippines
+		</p>
+
+	</div>
 </body>
 </html>
