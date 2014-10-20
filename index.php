86fe7ca74a067a87f298c2add665e02f4b50d4c4
Bien Jerico Cueto <bien.jerico@gmail.com>
2014-10-20 17:06:45 +0800

    with nav update

--- index.php
+++ index.php
@@ -7,197 +7,284 @@
     <title>bienjerico</title>
     <!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+  	<link rel="stylesheet" href="assets/css/style.css">
+
+	<script src="assets/js/modernizr.js"></script>
+
 </head>	
 <body>
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	 <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="assets/js/responsive-nav.js"></script>
+<div class="wrapper">
+
+	<main>
+
 
 	<div class="text-center">
 
+	<header>
+      <nav class="nav-collapse">
+        <ul>
+	        <li class="menu-item"><a href="#about" data-scroll><b>About</b></a></li>
+            <li class="menu-item"><a href="#skills" data-scroll><b>Skills</b></a></li>
+            <li class="menu-item"><a href="#experience" data-scroll><b>Experience</b></a></li>
+            <li class="menu-item"><a href="#projects" data-scroll><b>Projects</b></a></li>
+            <li class="menu-item"><a href="#certificate" data-scroll><b>Certificate</b></a></li>
+            <li class="menu-item"><a href="#contact" data-scroll><b>Contact</b></a></li>
+        </ul>
+      </nav>
+    </header>
+        
+            
+
+				
+
+		<section class="module parallax parallax-1">
+        <div class="container" id="about">
+			<h2><span class="label label-default">About <b class="text-primary">bienjerico</b></span></h2>
+				<p>
+					Is twenty plus of age and five-nine tall with slim body type
+					<br/>
+					Is a graduate of Informatics Internation College, Northgate Alabang and currently working at Yondu
+					<br/>
+					Is interested in travelling around the world, road trip, camping, island hopping, bar hopping and playing Dota One to Two and league of legends
+					<br/>
+					Is accepting layout, graphic, web, logo, invitation, t-shirt, car designs
+				</p>
+		</div>
+		</section>
+		
+		<section class="module content">
+        <div class="container"  id="skills">
+
+			<h2><span class="label label-default">Skills</span></h2>
+				<p>
+					<b class="text-primary">Programming</b>
+				   	<br/>
+					HTML5, PHP 5, CSS3, Javascript, Ajax, JSON, jQuery, Shell Script, API
+				</p>
+
+				<p>
+					<b class="text-primary">Design</b>
+					<br/>
+					Adobe CS3 and CS5 (Photoshop, InDesign, Dreamweaver, Illustrator, After Effects, Premiere, Lightroom, Flash)
+				</p>
+
+				<p>
+					<b class="text-primary">Database</b>
+					<br/>
+					MySQL, MSSQL, Oracle: 9i, MS Access
+				</p>
+
+				<p>
+					<b class="text-primary">Tools</b>
+					<br/>
+					XAMPP, LAMP, IIS7
+					<br/>
+					Sublime Text 2, Netbeans 7, WebMatrix, Notepad++, MySQL Workbench
+					<br/> 
+					SVN and GIT (version control), PuTTY, FileZilla, WinSCP, VMware Player
+					<br/>
+					Microsoft SQL Server, Microsoft Visual Studio 2008 R2, Office Word, Excel, Vision, PowerPoint and Outlook
+				</p>
+
+				<p>
+					<b class="text-primary">OS</b>
+				   	<br/>
+					Window XP, 7, 8 and Ubuntu
+				</p>
+		</div>
+		</section>
 			
-		<h3><span class="label label-default">About <b class="text-primary">bienjerico</b></span></h3>
+
+
+
+		<section class="module parallax parallax-2">
+        <div class="container" id="experience">	
+				<h2><span class="label label-default">Experience</span></h2>
+				<p>	
+					More than 3 years experienced as a open source web application developer. 
+					<br/>
+					Background experienced in font-end/back-end object oriented programming with MySQL database. 
+					<br/>
+					My Objective is to learn more latest web technologies and integrate in different projects and environment.
+				</p>
+
+				<p>
+					<b class="text-primary">Yondu</b>
+					<br/>
+					<b>Software Engineer</b> September 2014 to Present <i>(1 month)</i>
+					<br/>
+					Developed and designed web application and database structure. Research and provide new technology for system development. 
+				</p>
+
+				<p>
+					<b class="text-primary">BPO International, Inc.</b>
+					<br/>
+					<b>Senior Software Engineer</b> February 2013 to August 2014 <i>(1 year and 7 months)</i>
+					<br/>
+					<b>Software Engineer</b> April 2011 to October 2012 <i>(1 year and 7 months)</i>
+					<br/>
+					<b>On the Job Training</b> February 2011 to April 2013 <i>(3 months)</i>
+					<br/>
+					Developed and designed web application and database structure. Troubleshoot and maintain the existing system. 
+					<br/>
+					Setup and administer database client databases. Provide system update report. Research new idea for system development. 
+					<br/>
+					Provide new functionalities to enhancement of system. Analyze the system flow. 
+				</p>
+
+				<p>
+					<b class="text-primary">Samsung Electronics Philippines Corporation (SEPCO)</b>
+					<br/>
+					<b>Web Developer</b> November 2012 to January 2013 <i>(3 months)</i>
+					<br/>
+					Troubleshoot in-house web application. Update existing system. Developed web application and designed user interface. 
+					<br/>
+					Create and designed database structure.
+				</p>
+		</div>
+		</section>
+
+
+		<section class="module content">
+        <div class="container" id="projects">
+
+			<h2><span class="label label-default">Projects</span></h2>
 			<p>
-				Is twenty plus of age and five-nine tall with slim body type
+				<b class="text-primary">Captive Portal</b> <b>Yondu</b>
 				<br/>
-				Is a graduate of Informatics Internation College, Northgate Alabang and currently working at Yondu
+				On going project...
 				<br/>
-				Is interested in travelling around the world (dream), road trip, camping, island hopping, bar hopping and playing Dota One to Two and league of legends
+				<note><i>PHP 5, HTML 5, CSS3, Javascript (Ajax and jQuery), MySQL, Apache</i></note>
 				<br/>
-				Is accepting layout, graphic, web, logo, invitation, t-shirt, car designs
+				<br/>
+
+				<b class="text-primary">Time and Attendance System</b> <b>BPO International, Inc.</b>
+				<br/>
+				Combination of Leave Management Module, Overtime Management Module, Schedule Management Module, Timekeeping Module. 
+				<br/>
+				System provide PDF, Excel reports and email notification in every transaction. 
+				<br/>
+				Client can access the system using internet.
+				<br/>
+				<note><i>PHP 5 (PHP Native with Object Oriented Programming), HTML 5, CSS3 (Twitter Bootstrap), Javascript (Ajax and jQuery), MySQL, Apache, STMP</i></note>
+				<br/>
+				<br/>
+
+				<b class="text-primary">Employee Connection (E-CON)</b> <b>BPO International, Inc.</b>
+				<br/>
+				System developed to show updated news, photos, birthday celebrant, employee feedback module, office directory and also connected to Time and Attendance System.
+				<br/>
+				<note><i>PHP 5 (PHP Native with Object Oriented Programming), HTML 5, CSS3 (Twitter Bootstrap), Javascript (Ajax and jQuery), MySQL, Apache, STMP</i></note>
+				<br/>
+				<br/>
+				
+				<b class="text-primary">Online Report System</b> <b>BPO International, Inc.</b>
+				<br/>
+				System provide final report (Leaves, Balance, Man-Hours Work, Absenteeism, Pay Slip PDF and Credit Profile PDF) from Time and Attendance System. 
+				<br/>
+				Some of the client use ASP.NET for their company system, using SOAP can access the system.
+				<br/>
+				<note><i>PHP 5 (PHP Native with Object Oriented Programming), HTML 5, CSS3, Javascript (Ajax and jQuery), MySQL, Apache</i></note>
+				<br/>
+				<br/>
+
+				<b class="text-primary">Account Payable and Expenses Report Monitoring System</b> <b>BPO International, Inc.</b>
+				<br/>
+				System developed to track and monitor the status of client invoice. 
+				<br/>
+				This system use by Receiver, Processor and Reviewer to process the invoice. 
+				<br/>
+				<note><i>PHP 5 (PHP Native with Object Oriented Programming), HTML 5, CSS3, Javascript (Ajax and jQuery), MySQL, Apache</i></note>
+				<br/>
+				<br/>
+				
+				<b class="text-primary">Consumer Training Program</b> <b>SEPCO</b>
+				<br/>
+				Online training registration for the consumer.
+				<br/>
+				<note><i>PHP 5 (ADOdb Library), HTML 5, CSS3, Javascript (Ajax and jQuery), MSSQL, IIS7 and Microsoft Visual Studio 2008 R2.</i></note>
+
+			</p>
+		</div>
+		</section>
+
+
+		<section class="module parallax parallax-3">
+        <div class="container" id="certificate">
+
+			<h2><span class="label label-default">Certificate</span></h2>
+			<p>
+				<b class="text-primary">Microsoft Certified Professional</b>
+				<br/>
+				Programming in HTML5 with JavaScript and CSS3 Specialist <i>February 2013</i>
+			</p>
+
+			<p>
+				<b class="text-primary">Phoenix One - The Knowledge Institute</b>
+				<br/>
+				Effective Business Communication <i>September 2013</i>
 			</p>
 		
-		<h3><span class="label label-default">Skills</span></h3>
+		</div>
+		</section>
+
+		<section class="module content">
+        <div class="container" id="contact">
+			<h2><span class="label label-default">Contact</span></h2>
+
 			<p>
-				<b class="text-primary">Programming</b>
-			   	<br/>
-				HTML5, PHP 5, CSS3, Javascript, Ajax, JSON, jQuery, Shell Script, API
+				<b class="text-primary">Skype</b>
+				<br/>
+				bien.jerico.cueto
 			</p>
 
 			<p>
-				<b class="text-primary">Design</b>
+				<b class="text-primary">Email Address</b>
 				<br/>
-				Adobe CS3 and CS5 (Photoshop, InDesign, Dreamweaver, Illustrator, After Effects, Premiere, Lightroom, Flash)
-			</p>
-
-			<p>
-				<b class="text-primary">Database</b>
-				<br/>
-				MySQL, MSSQL, Oracle: 9i, MS Access
-			</p>
-
-			<p>
-				<b class="text-primary">Tools</b>
-				<br/>
-				XAMPP, LAMP, IIS7
-				<br/>
-				Sublime Text 2, Netbeans 7, WebMatrix, Notepad++, MySQL Workbench
-				<br/> 
-				SVN and GIT (version control), PuTTY, FileZilla, WinSCP, VMware Player
-				<br/>
-				Microsoft SQL Server, Microsoft Visual Studio 2008 R2, Office Word, Excel, Vision, PowerPoint and Outlook
-			</p>
-
-			<p>
-				<b class="text-primary">OS</b>
-			   	<br/>
-				Window XP, 7, 8 and Ubuntu
+				bien.jerico@gmail.com
 			</p>
 			
+			<p>
+				<b class="text-primary">Address</b>
+				<br/>
+				Philippines
+			</p>
+		</div>
+		</section>
 
-
-
-
-		<h3><span class="label label-default">Experience</span></h3>
-		<p>	
-			More than 3 years experienced as a open source web application developer. 
-			<br/>
-			Background experienced in font-end/back-end object oriented programming with MySQL database. 
-			<br/>
-			My Objective is to learn more latest web technologies and integrate in different projects and environment.
-		</p>
-
-		<p>
-			<b class="text-primary">Yondu</b>
-			<br/>
-			<b>Software Engineer</b> September 2014 to Present <i>(1 month)</i>
-			<br/>
-			Developed and designed web application and database structure. Research and provide new technology for system development. 
-		</p>
-
-		<p>
-			<b class="text-primary">BPO International, Inc.</b>
-			<br/>
-			<b>Senior Software Engineer</b> February 2013 to August 2014 <i>(1 year and 7 months)</i>
-			<br/>
-			<b>Software Engineer</b> April 2011 to October 2012 <i>(1 year and 7 months)</i>
-			<br/>
-			<b>On the Job Training</b> February 2011 to April 2013 <i>(3 months)</i>
-			<br/>
-			Developed and designed web application and database structure. Troubleshoot and maintain the existing system. 
-			<br/>
-			Setup and administer database client databases. Provide system update report. Research new idea for system development. 
-			<br/>
-			Provide new functionalities to enhancement of system. Analyze the system flow. 
-		</p>
-
-		<p>
-			<b class="text-primary">Samsung Electronics Philippines Corporation (SEPCO)</b>
-			<br/>
-			<b>Web Developer</b> November 2012 to January 2013 <i>(3 months)</i>
-			<br/>
-			Troubleshoot in-house web application. Update existing system. Developed web application and designed user interface. 
-			<br/>
-			Create and designed database structure.
-		</p>
-
-
-		<h3><span class="label label-default">Projects</span></h3>
-		<p>
-			<b class="text-primary">Captive Portal</b> <b>Yondu</b>
-			<br/>
-			On going project...
-			<br/>
-			<h6><i>&nbsp;</i></h6>
-			<br/>
-
-			<b class="text-primary">Time and Attendance System</b> <b>BPO International, Inc.</b>
-			<br/>
-			Combination of Leave Management Module, Overtime Management Module, Schedule Management Module, Timekeeping Module. 
-			<br/>
-			System provide PDF, Excel reports and email notification in every transaction. 
-			<br/>
-			Client can access the system using internet.
-			<br/>
-			<h6><i>PHP 5 (PHP Native with Object Oriented Programming), HTML 5, CSS3 (Twitter Bootstrap), Javascript (Ajax and jQuery), MySQL, Apache, STMP</i></h6>
-			<br/>
-			
-			<b class="text-primary">Employee Connection (E-CON)</b> <b>BPO International, Inc.</b>
-			<br/>
-			System developed to show updated news, photos, birthday celebrant, employee feedback module, office directory and also connected to Time and Attendance System.
-			<br/>
-			<h6><i>PHP 5 (PHP Native with Object Oriented Programming), HTML 5, CSS3 (Twitter Bootstrap), Javascript (Ajax and jQuery), MySQL, Apache, STMP</i></h6>
-			<br/>
-			
-			<b class="text-primary">Online Report System</b> <b>BPO International, Inc.</b>
-			<br/>
-			System provide final report (Leaves, Balance, Man-Hours Work, Absenteeism, Pay Slip PDF and Credit Profile PDF) from Time and Attendance System. 
-			<br/>
-			Some of the client use ASP.NET for their company system, using SOAP can access the system.
-			<br/>
-			<h6><i>PHP 5 (PHP Native with Object Oriented Programming), HTML 5, CSS3, Javascript (Ajax and jQuery), MySQL, Apache</i></h6>
-			<br/>
-
-			<b class="text-primary">Account Payable and Expenses Report Monitoring System</b> <b>BPO International, Inc.</b>
-			<br/>
-			System developed to track and monitor the status of client invoice. 
-			<br/>
-			This system use by Receiver, Processor and Reviewer to process the invoice. 
-			<br/>
-			<h6><i>PHP 5 (PHP Native with Object Oriented Programming), HTML 5, CSS3, Javascript (Ajax and jQuery), MySQL, Apache</i></h6>
-			<br/>
-			
-			<b class="text-primary">Consumer Training Program</b> <b>SEPCO</b>
-			<br/>
-			Online training registration for the consumer.
-			<br/>
-			<h6><i>PHP 5 (ADOdb Library), HTML 5, CSS3, Javascript (Ajax and jQuery), MSSQL, IIS7 and Microsoft Visual Studio 2008 R2.</i></h6>
-
-		</p>
-
-
-		<h3><span class="label label-default">Certificate</span></h3>
-		<p>
-			<b class="text-primary">Microsoft Certified Professional</b>
-			<br/>
-			Programming in HTML5 with JavaScript and CSS3 Specialist <i>February 2013</i>
-		</p>
-
-		<p>
-			<b class="text-primary">Phoenix One - The Knowledge Institute</b>
-			<br/>
-			Effective Business Communication <i>September 2013</i>
-		</p>
-
-		<h3><span class="label label-default">Contact</span></h3>
-
-		<p>
-			<b class="text-primary">Skype</b>
-			<br/>
-			bien.jerico.cueto
-		</p>
-
-		<p>
-			<b class="text-primary">Email Address</b>
-			<br/>
-			bien.jerico@gmail.com
-		</p>
-		
-		<p>
-			<b class="text-primary">Address</b>
-			<br/>
-			Philippines
-		</p>
 
 	</div>
+
+	</main>
+</div>
+<script src="assets/js/fastclick.js"></script>
+<script src="assets/js/scroll.js"></script>
+<script src="assets/js/fixed-responsive-nav.js"></script>
+<!---
+<script>
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});-->
+</script>
 </body>
 </html>
