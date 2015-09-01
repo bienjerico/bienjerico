bd0af661f4d3f59399658d1c750ed4734b453384
Bien Jerico Cueto <bien.jerico@gmail.com>
2015-09-01 11:50:58 +0800

    remove the modal

--- index.php
+++ index.php
@@ -148,13 +148,13 @@
             
             <!-- Trigger the modal with a button -->
             
-<div class="square" data-toggle="modal" data-target="#myModal">Open Modal</div>
+<!--<div class="square" data-toggle="modal" data-target="#myModal">Open Modal</div>-->
 
 <!-- Modal -->
-<div id="myModal" class="modal fade" role="dialog">
+<!--<div id="myModal" class="modal fade" role="dialog">
   <div class="modal-dialog">
 
-    <!-- Modal content-->
+     Modal content
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
@@ -169,7 +169,7 @@
     </div>
 
   </div>
-</div>
+</div>-->
 
 
 
