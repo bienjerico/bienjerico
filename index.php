c4ea6863f2da48d5e0f1748e7edd772d0bba3745
Bien Jerico Cueto <bien.jerico@gmail.com>
2016-07-12 19:58:51 +0800

    google analystics

--- index.php
+++ index.php
@@ -325,6 +325,14 @@
     $(".label-name").hide().delay(200).fadeIn(400);
   });
 
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-80629363-1', 'auto');
+    ga('send', 'pageview');
+
 
 </script>
 </body>
